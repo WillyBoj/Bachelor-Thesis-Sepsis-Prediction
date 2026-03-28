2cdac2152196205bc52ad714e90a1507af6c2439
William Bojstrup <Hejwilliam10@gmail.com>
2026-03-28 11:50:38 +0100

    Add files via upload

--- Sepsis_logistic_reg.R
+++ Sepsis_logistic_reg.R
@@ -2,9 +2,7 @@ library(tidyverse)
 library(tidymodels)
 library(ggcorrplot)
 library(slider)
-library(pROC)
-#----------------recipe----------------------------------
-#---------------------------------------------------------
+#exclusion of variables --------------------------------------------------------
 excluded_variables <- c(
   "SBP",
   "DBP",
@@ -32,6 +30,9 @@ excluded_variables <- c(
   "Unit2"
 )
 
+#------------------------------------------------------------------------
+#Defining forward fill function-----------------------------------------
+
 ffill_dataset <- function(df) {
   df %>%
     arrange(patient_id, ICULOS) %>%
@@ -39,6 +40,9 @@ ffill_dataset <- function(df) {
     fill(-patient_id, -ICULOS, .direction = "down") %>%   # forward-fill
     ungroup()
 }
+
+#------------------------------------------------------------------------
+#defining missing indicator creation function ---------------------------
 
 add_indicator_columns <- function(df, outcome = "SepsisLabel") {
   No_indicator_variables <- c("patient_id", "ICULOS", outcome)
@@ -51,7 +55,8 @@ add_indicator_columns <- function(df, outcome = "SepsisLabel") {
     ))
 }
 
-
+#------------------------------------------------------------------------
+#defining add rolling features to dataset function -----------------------
 
 add_rolling_features <- function(df) {
   df %>%
@@ -72,13 +77,17 @@ add_rolling_features <- function(df) {
     ungroup()
 }
 
+#------------------------------------------------------------------------
 
-train_preprocess_ffill <- train %>%
-  select(-any_of(excluded_variables)) %>%
-  add_indicator_columns(outcome = "SepsisLabel") %>%
-  ffill_dataset() %>%
-  add_rolling_features() %>% 
-  mutate(SepsisLabel = as.factor(SepsisLabel))
+#Applying the function to preprocessing of the training and test dataset
+
+train_preprocess_ffill <- train %>%                    
+  select(-any_of(excluded_variables)) %>%                 #removing excluded variables
+  add_indicator_columns(outcome = "SepsisLabel") %>%      #adding missing data indicators
+  ffill_dataset() %>%                                     #Forward filling the missing data
+  add_rolling_features() %>%                              #adding rolling features
+  mutate(SepsisLabel = as.factor(SepsisLabel)) %>%        #making sure
+  mutate(Gender = as.factor(Gender))
 
 
 test_preprocess_ffill <- test %>%
@@ -86,71 +95,73 @@ test_preprocess_ffill <- test %>%
   add_indicator_columns(outcome = "SepsisLabel") %>%
   ffill_dataset() %>%
   add_rolling_features() %>% 
-  mutate(SepsisLabel = as.factor(SepsisLabel))
-
+  mutate(SepsisLabel = as.factor(SepsisLabel))%>%        #making sure
+  mutate(Gender = as.factor(Gender))
 
 train_preprocess_ffill %>% count(SepsisLabel)
 
+#--------------------------------------------------------
+#----------------recipe----------------------------------
+#---------------------------------------------------------
 
 sepsis_recipe_log <- recipe(SepsisLabel ~ ., data = train_preprocess_ffill) %>%
   step_rm(patient_id) %>%
+  step_dummy(Gender) %>% 
   step_impute_mean(all_numeric_predictors())
+
+#Model - mode and Engine---------------------------------
 
 logistic_sepsis_model <- logistic_reg() %>%
   set_engine('glm') %>%
   set_mode('classification')
+#-----------------------------------------------------------
 
+#Prep and baking of data into Training and Test dataset ----
 Sepsis_log_prep <- sepsis_recipe_log %>%
   prep(training = train_preprocess_ffill)
 
-Sepsis_log_training_prep <- Sepsis_log_prep %>%   # ← Sepsis_log_prep, ikke sepsis_recipe_log
+Sepsis_log_training_prep <- Sepsis_log_prep %>% 
   bake(new_data = NULL)
 
 Sepsis_log_test_prep <- Sepsis_log_prep %>%
   bake(new_data = test_preprocess_ffill)
 
+#-----------------------------------------------------------
+
+# Fitting model -------------------------------------------
 Logistic_sepsis_fit <- logistic_sepsis_model %>%
   fit(SepsisLabel~., data = Sepsis_log_training_prep)
 
+#-----------------------------------------------------------
+
+#preparing preditions --------------------------------------------------------
 Class_preds_sepsis <- predict(Logistic_sepsis_fit, new_data = Sepsis_log_test_prep, type = "class")
 
 prob_preds_sepsis <- predict(Logistic_sepsis_fit, new_data = Sepsis_log_test_prep, type = "prob")
+#-----------------------------------------------------------
 
+#Results of the model --------------------------------------
 sepsis_results <- test_preprocess_ffill %>%
   select(SepsisLabel) %>%
   bind_cols(Class_preds_sepsis, prob_preds_sepsis)
 
 sepsis_results
+#-----------------------------------------------------------
 
-test_preprocess_ffill <- test_preprocess_ffill %>%
-  mutate(Sepsis_Preds = prob_preds_sepsis$.pred_1*100)
+# test_preprocess_ffill <- test_preprocess_ffill %>%
+#  mutate(Sepsis_Preds = prob_preds_sepsis$.pred_1*100)
 
-####CLAUDE####
 
-# Get the ROC curve with all possible thresholds
-roc_data <- roc_curve(sepsis_results, 
-                      truth = SepsisLabel, 
-                      .pred_1, 
-                      event_level = "second")
+#Evalutation --------------------------------------------------
+library(DescTools)
+BrierScore(sepsis_results$SepsisLabel == "1", sepsis_results$.pred_1)
 
-# Find optimal threshold using Youden's J (maximizes sens + spec - 1)
-roc_data <- roc_data %>%
-  mutate(j_index = sensitivity + specificity - 1)
+prediction_eval <- metric_set(roc_auc, brier_class)
 
-best_threshold <- roc_data %>%
-  slice_max(j_index, n = 1)
+prediction_eval(sepsis_results, truth = SepsisLabel, .pred_1, event_level = "second")
 
-best_threshold
+conf_mat(sepsis_results, truth = SepsisLabel, .pred_class)
 
-sepsis_results <- sepsis_results %>%
-  mutate(.pred_class_optimal = factor(
-    ifelse(.pred_1 > best_threshold$.threshold, 1, 0),
-    levels = levels(SepsisLabel)
-  ))
-##########
 
-prediction_eval <- metric_set(accuracy, sens, spec, roc_auc, brier_class)
+#-----------------------------------------------------------
 
-prediction_eval(sepsis_results, truth = SepsisLabel, estimate=.pred_class_optimal,.pred_1, event_level = "second")
-
-conf_mat(sepsis_results, truth = SepsisLabel, estimate=.pred_class_optimal)
